9854199cf5a15fc0b1fae839616f5475d7dacdba
Pranav Ramesh <laphatize@protonmail.com>
2025-04-28 01:55:37 -0400

    feat more cors pain

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const io = socketIo(server, {
 
 // Increase JSON body size limit to 10mb for image uploads
 app.use(express.json({ limit: '10mb' }));
-app.use(cors({ origin: ['*'] }));
+app.use(cors({ origin: "https://barscout.ctfguide.com" }));
 // DB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
